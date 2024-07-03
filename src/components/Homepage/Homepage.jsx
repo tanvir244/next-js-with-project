ee36d2d253871a607339722a0b18aa6e290fba97
tanvir244 <tr30365s25@gmail.com>
2024-07-03 15:29:44 +0600

    project setup and navbar footer complete

---
+++ src/components/Homepage/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Homepage = () => {
+    return (
+        <div className='h-screen'>
+            Home Page
+        </div>
+    );
+};
+
+export default Homepage;
